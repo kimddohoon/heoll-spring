4de284f14decac08153c733247b46977dff2fc1c
woohyeun <docter4@naver.com>
2021-09-17 22:08:02 +0900

    Initial commit

---
+++ src/main/java/hello/heollspring/HeollSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.heollspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeollSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeollSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/heollspring/HeollSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.heollspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeollSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
